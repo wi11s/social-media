c7fc3555d1acf034301b4e7f3bf74ff8375aed4e
William <wms274@nyu.edu>
2022-11-14 11:08:05 -0500

    auth

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -2,6 +2,12 @@ import React from 'react'
 
 export default function Login() {
   return (
-    <div>Login</div>
+    <div>
+        <form>
+            <input type='text' placeholder='username' />
+            <input type='password' placeholder='password' />
+            <button type='submit'>Login</button>
+        </form>
+    </div>
   )
 }
